a1665613474a95abed0333e6b89cc0323bf137b5
Iron318i <iron318i@gmail.com>
2022-11-09 16:20:25 +0300

    16-20

--- template-parts/lp-header.php
+++ template-parts/lp-header.php
@@ -20,5 +20,35 @@ defined('ABSPATH') || exit;
                 </svg>
                 01252 888 177</a>
         </div>
+        <ul class="social">
+            <li>
+                <a href="https://www.facebook.com/ConfidentalUK/" target="_blank">
+                    <svg class="icon">
+                        <use xlink:href="#facebook"></use>
+                    </svg>
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/ConfidentalUK?lang=en" target="_blank">
+                    <svg class="icon">
+                        <use xlink:href="#twitter"></use>
+                    </svg>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.youtube.com/channel/UCIgPWkLc9EVtJK6Mprf_Few" target="_blank">
+                    <svg class="icon">
+                        <use xlink:href="#youtube"></use>
+                    </svg>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/confidental_smiles/" target="_blank">
+                    <svg class="icon">
+                        <use xlink:href="#instagram"></use>
+                    </svg>
+                </a>
+            </li>
+        </ul>
     </div>
 </div>
