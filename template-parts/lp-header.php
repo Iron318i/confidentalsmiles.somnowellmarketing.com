37f561f5ce84fab1f571cfd25face0315e333ecd
Iron318i <iron318i@gmail.com>
2022-11-10 10:36:28 +0300

    Header

--- template-parts/lp-header.php
+++ template-parts/lp-header.php
@@ -9,9 +9,11 @@ defined('ABSPATH') || exit;
 ?>
 <div class="lp-header">
     <div class="container">
-        <a href="<?php echo site_url() ?>" class="main-logo"><img src="<?php echo get_template_directory_uri() ?>/images/lp/main-logo.png" alt="<?php bloginfo('name'); ?>"></a>
-        <div class="slogan">
-            70A YorkTown Road, Sandhurst, Berkshire, GU47 9BT
+        <div class="logo-wrp">
+            <a href="<?php echo site_url() ?>" class="main-logo"><img src="<?php echo get_template_directory_uri() ?>/images/lp/main-logo.png" alt="<?php bloginfo('name'); ?>"></a>
+            <div class="slogan">
+                70A YorkTown Road, Sandhurst, Berkshire, GU47 9BT
+            </div>
         </div>
         <div class="phone">
             <a href="tel:01252888177">
