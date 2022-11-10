de498bdf467cbe027c773741ff939f8cff422ff7
Iron318i <iron318i@gmail.com>
2022-11-10 10:54:09 +0300

    10-50

--- page-templates/lp-smile-makeover.php
+++ page-templates/lp-smile-makeover.php
@@ -7,6 +7,13 @@ get_header();
 echo '<div class="lp-wrp">';
 get_template_part('template-parts/lp-header');
 ?>
+    <section class="main-content">
+        <div class="container">
+            <div class="top-heading">
+                <h2 class="text-primary">Transform your teeth and your confidence <br>for life by addressing the aspects of your smile <br><strong>you’d like to change</strong></h2>
+            </div>
+        </div>
+    </section>
 <?php
 echo ' </div>';
 get_template_part('template-parts/svg-sprite');
