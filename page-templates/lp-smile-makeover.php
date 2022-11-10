358e000657a11196f03edb65ebb4508ca94f636e
Iron318i <iron318i@gmail.com>
2022-11-10 15:09:44 +0300

    15-00

--- page-templates/lp-smile-makeover.php
+++ page-templates/lp-smile-makeover.php
@@ -7,10 +7,23 @@ get_header();
 echo '<div class="lp-wrp">';
 get_template_part('template-parts/lp-header');
 ?>
-    <section class="main-content">
+    <section class="top-heading py-5">
         <div class="container">
-            <div class="top-heading">
-                <h2 class="text-primary">Transform your teeth and your confidence <br>for life by addressing the aspects of your smile <br><strong>you’d like to change</strong></h2>
+            <h3 class="text-primary">Transform your teeth and your confidence <br>for life by addressing the aspects of your smile <br><strong>you’d like to change</strong></h3>
+        </div>
+    </section>
+    <section class="what-is pt-5 pb-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-5">
+                    <img src="https://www.confidentalsmiles.co.uk/wp-content/uploads/2022/11/what-is-smile-makeover.jpg" alt="What is Smile Makeover?">
+                </div>
+                <div class="col-md-7">
+                    <h2 class="h2 mb-2">What is Smile Makeover?</h2>
+                    <p class="mb-2">A Smile Makeover encompasses multiple cosmetic dental treatments. Restorative and orthodontic treatments such as <a href="http://confidentalsmiles.somnowellmarketing.com/treatment/dental-implants/">dental implants</a> and <a href="http://confidentalsmiles.somnowellmarketing.com/treatment/invisalign/">braces</a> can also be included as part of your Smile Makeover.</p>
+                    <p class="mb-2">It may be that you wish to improve the shade and shape of certain teeth with <a href="http://confidentalsmiles.somnowellmarketing.com/treatment/teeth-whitening/">teeth whitening</a> and <a href="http://confidentalsmiles.somnowellmarketing.com/treatment/veneers/">veneers</a>, or perhaps you are looking for minimally-invasive solutions to fix multiple chipped and decayed teeth with <a href="http://confidentalsmiles.somnowellmarketing.com/treatment/composite-bonding/">composite bonding</a> and <a href="http://confidentalsmiles.somnowellmarketing.com/treatment/crowns-bridges/">crowns</a>.</p>
+                    <p>Your Teeth Makeover will be carefully planned by your dentist based on your individual needs. With the cutting-edge digital planning software available to us, we can reach a predictable outcome that we are sure you will be thrilled with.</p>
+                </div>
             </div>
         </div>
     </section>
