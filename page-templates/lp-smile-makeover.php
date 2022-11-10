33e49c8877ca93023591339b26aa599df4b41cd1
Iron318i <iron318i@gmail.com>
2022-11-10 16:39:06 +0300

    16-40

--- page-templates/lp-smile-makeover.php
+++ page-templates/lp-smile-makeover.php
@@ -27,7 +27,7 @@ get_template_part('template-parts/lp-header');
             </div>
         </div>
     </section>
-    <section class="sm-sm-benefits pt-5 pb-4">
+    <section class="sm-benefits py-5">
         <div class="container">
             <h2 class="h2 mb-2">Smile Makeover Benefits</h2>
             <div class="row">
@@ -60,6 +60,52 @@ get_template_part('template-parts/lp-header');
                         <use xlink:href="#check"></use>
                     </svg>
                     <p>Achieve a symmetrical and natural-looking smile</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="why-choose-cs pb-4">
+        <svg width="0" height="0" class="d-none">
+            <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 22.01" id="crown" fill="currentColor">
+                <path d="M24,9a3,3,0,0,0-6,0,3.05,3.05,0,0,0,.06.6l-1.65,1L14.15,6.08a3,3,0,1,0-4.3,0L7.59,10.59l-1.65-1A3.05,3.05,0,0,0,6,9a3,3,0,1,0-3.68,2.91l.6,5.41A2,2,0,0,0,2,19v2a2,2,0,0,0,2,2H20a2,2,0,0,0,2-2V19a2,2,0,0,0-.92-1.68l.6-5.41A3,3,0,0,0,24,9ZM21,8a1,1,0,1,1-1,1A1,1,0,0,1,21,8ZM12,3a1,1,0,1,1-1,1A1,1,0,0,1,12,3ZM8,13a.9.9,0,0,0,.28,0,1,1,0,0,0,.62-.51L11.64,7A2.2,2.2,0,0,0,12,7a2.2,2.2,0,0,0,.36,0l2.74,5.49a1,1,0,0,0,.62.51A.9.9,0,0,0,16,13a1,1,0,0,0,.51-.14l2.59-1.55a2.9,2.9,0,0,0,.6.38L19.1,17H4.89L4.3,11.69a2.83,2.83,0,0,0,.59-.38l2.59,1.55A1.07,1.07,0,0,0,8,13ZM3,8A1,1,0,1,1,2,9,1,1,0,0,1,3,8ZM4,21V19H20v2Z" transform="translate(0 -0.99)"></path>
+            </symbol>
+            <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 18" id="eye" fill="currentColor">
+                <path d="M23.91,11.58C21.94,7.31,17.5,3,12,3S2.06,7.31.09,11.58a1,1,0,0,0,0,.84C2.06,16.69,6.5,21,12,21s9.94-4.31,11.91-8.58A1,1,0,0,0,23.91,11.58ZM12,19c-4.41,0-8.15-3.55-9.89-7C3.85,8.55,7.59,5,12,5s8.15,3.55,9.89,7C20.15,15.45,16.41,19,12,19Z" transform="translate(0 -3)"></path>
+                <path d="M12,7a5,5,0,1,0,5,5A5,5,0,0,0,12,7Zm0,8a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z" transform="translate(0 -3)"></path>
+            </symbol>
+            <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 511.15 448" id="diamond" fill="currentColor">
+                <path d="M507.9,196.4,403.9,42.6A25.21,25.21,0,0,0,384,32H127.1c-7.1,0-14.5,4-19,10.56L5,196.36a24,24,0,0,0,2.41,29.89l231.1,246.2a24,24,0,0,0,35,0l232-246.2A23.88,23.88,0,0,0,507.9,196.4ZM382.5,96.59,446.1,192H306ZM256,178.9,177.6,80H334.3ZM129.5,96.59,205.1,192H65ZM256,421,85.42,240h341.2Z" transform="translate(-0.87 -32)"></path>
+            </symbol>
+        </svg>
+        <div class="container">
+            <h2 class="h2 mb-2 text-center">Why Choose Confident Smiles <br> For Your Smile Makeover?</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="icon-wrp mb-2">
+                        <svg class="icon">
+                            <use xlink:href="#diamond"></use>
+                        </svg>
+                    </div>
+                    <p><strong class="fsz-18">Premium materials:</strong><br>
+                        We use durable, high-quality materials to create restorations that will transform your smile.</p>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-wrp mb-2">
+                        <svg class="icon">
+                            <use xlink:href="#eye"></use>
+                        </svg>
+                    </div>
+                    <p><strong class="fsz-18">A commitment to detail:</strong><br>
+                        We perform an extensive analysis of what makes your smile unique (some might say we’re obsessives when it comes to detail!) It’s this philosophy that allows us to produce an aesthetically pleasing composition between your smile and your face as a whole.</p>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-wrp mb-2">
+                        <svg class="icon">
+                            <use xlink:href="#crown"></use>
+                        </svg>
+                    </div>
+                    <p><strong class="fsz-18">Experienced clinicians:</strong><br>
+                        Our clinicians have completed advanced training, allowing them to execute a result that not only looks attractive, but that upholds the health and overall wellbeing of your smile.</p>
                 </div>
             </div>
         </div>
