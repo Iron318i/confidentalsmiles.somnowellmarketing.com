868049db76affddda16af85290cdf2642452a1f5
Iron318i <iron318i@gmail.com>
2022-11-16 14:11:50 +0300

    14-11

---
+++ page-templates/lp-invisalign.php
@@ -0,0 +1,162 @@
+<?php
+/*
+  Template Name: LP - Invisalign
+  Template Post Type: page
+ */
+get_header();
+echo '<div class="lp-wrp lp-invisalign">';
+get_template_part('template-parts/lp-header');
+?>
+    <section class="lp-banner" style="background-image: url('https://www.confidentalsmiles.co.uk/wp-content/uploads/2022/11/lp-invisalign.jpg');">
+        <img src="https://www.confidentalsmiles.co.uk/wp-content/uploads/2022/11/lp-invisalign-sm.jpg" alt="" class="mb-1 hide-lg-up">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-md-5 content">
+                    <h1>Invisalign in <br> Sandhurst</h1>
+                    <a href="https://www.confidentalsmiles.co.uk/virtual-tour/" class=""><img src="<?php echo get_template_directory_uri() ?>/images/lp/virtual-tour.png" alt="Virtual Tour"></a>
+                </div>
+                <div class="col-lg-5 col-md-7">
+                    <h3 class="h3">Book a Free Smile Consultation</h3>
+                    <script type="text/javascript">
+                        hbspt.forms.create({
+                            region: "eu1",
+                            portalId: "25790036",
+                            formId: "fa0ac857-d050-4de3-afdc-491c310b2ad5"
+                        });
+                    </script>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="top-heading py-5">
+        <div class="container">
+            <h3 class="text-primary">Transform your teeth and your confidence <br>for life by addressing the aspects of your smile <br><strong>you’d like to change</strong></h3>
+        </div>
+    </section>
+    <section class="pb-4 center-mobile">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="https://www.confidentalsmiles.co.uk/wp-content/uploads/2022/11/Invisalign-logo.jpg" alt="Invisalign" class="mb-1">
+                    <h2 class="h2 mb-1">Start Your Invisalign <br> Treatment from as little <br> as £29.50 per month</h2>
+                </div>
+                <div class="col-md-6">
+                    <p class="mb-1">Virtually invisible Invisalign® Aligners that won’t hamper your style. Come and see our amazing results for yourself click here to see our smile gallery. </p>
+                    <p class="mb-1">If you have twisted, crooked or overlapping teeth and you keep hiding your smile, then Invisalign® could be for you! </p>
+                    <p class="mb-1">The great news is that the field of orthodontics has come a long way over the past few decades. Thanks to Invisalign® (the most popular US brand), it’s now possible to use near-invisible removable aligners or invisible braces instead of fixed braces to straighten your teeth.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="what-is-sm pt-5 pb-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-5">
+                    <img src="https://www.confidentalsmiles.co.uk/wp-content/uploads/2022/11/why-is-Invisalign.jpg" alt="Why is Invisalign right for you?">
+                </div>
+                <div class="col-md-7">
+                    <h2 class="h2 mb-2">Why is Invisalign® <br> right for you?</h2>
+                    <p class="mb-2">Clear, comfortable and effective.</p>
+                    <p class="mb-2"> Invisalign® is a clear removable brace system which is virtually invisible and is used to straighten mildly crooked or miss-aligned teeth. The revolutionary technique is becoming more and more popular as patients realise, they can get effective results without making it obvious that you are even wearing braces!</p>
+
+                    <p class="mb-2"> No metal! No wires!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-4 center-mobile">
+        <div class="container">
+            <h2 class="h2 mb-2 text-center">How does Invisalign® straighten my teeth?</h2>
+            <div class="ratio mb-2">
+                <iframe src="https://player.vimeo.com/video/283906373?rel=0&showinfo=0&dnt=1" title="Implants" allowfullscreen></iframe>
+            </div>
+            <p class="mb-2">No more embarrassing metal wires, due to near invisibility of Invisalign® braces.</p>
+            <p class="mb-2">No metal allergies due to brackets and wires.Greater treatment visibility and easier planning with our treatment software.They are tailored to match your teeth, for reduced irritation.</p>
+            <p class="mb-2">Our crystal-clear aligners from Invisalign® are ideal if you would like to straighten your teeth without anyone knowing. The clear aligners are moulded to fit your teeth and can be easily removed for eating, brushing your teeth and the occasional evening out, making them a popular choice with many people.</p>
+            <p class="mb-2">There are no sharp edges or individual brackets with Invisalign®, meaning they are comfortable and irritation-free. You will be welcomed back regularly to review progress as your custom-made aligners transform your smile by moving your teeth to their desired position little by little, whilst you hardly notice you are wearing them! The average treatment time with Invisalign® is 12 – 24 months.</p>
+            <p class="mb-2"><strong>Wearing the aligners:</strong> You wear each Invisalign® aligner for two weeks, before moving on to the next in the series, only taking them out to eat, drink, brush and floss your teeth as normal.</p>
+            <p class="mb-2"><strong>Follow up appointments:</strong> We will see you every 4-6 weeks during your treatment until your teeth are straightened into the positions that we have prescribed, and it is determined that your treatment is complete.
+            </p>
+            <h2 class="h2 mb-2">Helping to make your treatment affordable.</h2>
+            <p class="mb-2">Every case is different and your treatments journey may vary. As a guide treatments usually vary between £2500 and £4500. We offer payment plans to make your treatment more affordable. We can give you a specific breakdown of the cost when we get to see your mouth, but to give you an idea, Invisalign® treatment can cost as little as £29.50 per month using our payment plans. Please visit out Guarantee and Finance page where you will find an easy to use finance calculator.</p>
+
+        </div>
+    </section>
+    <section class="why-choose-cs pb-4">
+        <svg width="0" height="0" class="d-none">
+            <symbol id="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="currentColor">
+                <path d="M34,60.64c.11-.1,11.64-10.47,24.69-25.11,5.44-6.11,6.81-14,3.64-21C59.29,7.74,52.87,3.36,46,3.36s-11.35,3-14,6.31c-2.63-3.28-7.1-6.31-14-6.31S4.71,7.74,1.68,14.51c-3.17,7-1.8,14.91,3.64,21C18.37,50.17,29.9,60.54,30,60.64Zm.9-43.9C35.08,16,37.09,9.32,46,9.32c4.48,0,8.87,3.06,10.91,7.63,2.21,4.92,1.24,10.25-2.66,14.62C44.59,42.39,35.8,50.83,32,54.38c-3.8-3.55-12.58-12-22.23-22.81C5.87,27.2,4.9,21.87,7.11,17c2-4.57,6.43-7.64,10.91-7.64,8.88,0,10.89,6.64,11.09,7.42Z"></path>
+            </symbol>
+            <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.93 21.03" id="key">
+                <path d="M19.93,1l-2,2m-7.61,7.61A5.5,5.5,0,0,1,2.6,18.44l-.06-.05a5.5,5.5,0,0,1,7.78-7.78Zm0,0L14.43,6.5m0,0,3,3L20.93,6l-3-3m-3.5,3.5L17.93,3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+            </symbol>
+            <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 509.74 503.26" id="bridges" fill="currentColor" stroke="currentColor" stroke-width="20">
+                <path d="M506.48,355c-.38,19.67-5.7,37.84-14.18,55.31a33.13,33.13,0,0,0-2.64,10.88c-1.2,11.72-2,23.48-3.09,35.2-.57,6-4.25,10.15-9.4,12.71a86.18,86.18,0,0,1-51.83,8.34,10.36,10.36,0,0,0-6.7,1.35C409.39,485,399,487.7,388.23,489.3c-13.5,2-27.07,3.63-40.52-.68a4.14,4.14,0,0,0-3.6.8c-5.89,6.84-14.12,7.86-22.18,8.82-16.69,2-33.46,3.49-50.24,1.14-4.33-.61-8.45-2.6-12.71-3.83-1.6-.46-3.93-1.37-4.88-.66-6,4.47-13.12,5-20,5.3-18.15.86-36.27,0-54.09-3.85a22.4,22.4,0,0,1-10-5.14c-2.95-2.7-5.37-2.48-8.9-1.92C137,493.1,113.76,491,92.3,478.07a7.85,7.85,0,0,0-5-.83c-17.81,3.07-34.69-.09-51.07-7.18C25.49,465.4,24.33,455.64,23.48,446c-.8-9.1-.77-18.27-1.28-27.4a15.21,15.21,0,0,0-1.53-6.16C9.05,390.64,3.59,367.42,6.58,343c3.82-31.14,19.47-56.35,56.11-61,21.76-2.73,43.13-.87,64.46,2.83,34.57,6,67.83,16.42,100.17,30,18.83,7.9,38,8,56.84.13,30.65-12.82,62.13-22.88,94.76-29.09,23.91-4.55,47.93-7,72.34-3.64,26.56,3.7,45.36,21,52,49.63C505,339.43,505.43,347.29,506.48,355Zm-18.75,38.29a37.49,37.49,0,0,0,2-4c6.09-18,7.73-36.41,3.43-55.05-5.68-24.62-20.52-38.79-43.71-41.73-21.29-2.7-42.48-1.22-63.55,2.49-33.41,5.87-65.56,15.88-96.79,29.09-22.42,9.49-44.72,8.69-67.4-.22-19.95-7.83-40.21-15.1-60.8-21-31.81-9.05-64.26-14.46-97.56-10.49-18.11,2.15-32,10.45-39.72,27.71-10.36,23.21-9,46.58-1.26,70.07.32,1,1.32,1.68,2.32,2.9,1-2.27,1.55-3.65,2.18-5,4.66-10,15.81-16.24,26.69-14.63C66.23,375.35,77,381,85.2,390.93c1.11,1.34,2.35,2.58,3.89,4.27.9-1.67,1.5-2.61,1.93-3.62,5.32-12.31,14.86-18.84,28.17-18.06,17,1,31.63,7.47,42.57,21A22.89,22.89,0,0,1,164,399c1.78-1.75,2.09-1.93,2.25-2.2.81-1.45,1.55-2.94,2.38-4.38,5.73-10,14.3-16.49,25.67-17.95,25.75-3.31,48.22,9.84,58.84,33.69.7,1.58,1.45,3.14,2.75,5.92,1.38-2.9,2.18-4.61,3-6.31,10.56-22.13,27.88-33.71,52.61-33.68,14.7,0,25.37,6.81,32.34,19.63.75,1.37,1.6,2.68,2.5,4.19a10.38,10.38,0,0,0,2.05-1.5c10.59-13.67,24.51-21.28,41.73-22.54,13.54-1,25.88,4.12,30.32,16.75.47,1.35,1.21,2.6,1.84,3.95.79-.28,1.37-.32,1.68-.62a28.84,28.84,0,0,0,2.31-2.62c6.22-7.79,14.27-12.84,23.65-16,16.61-5.61,31.81,1.78,36.39,15.95A13.77,13.77,0,0,0,487.73,393.31ZM340.85,443.53c-.91-9.65-1.91-23.41-3.59-37.09-1.39-11.44-10.9-20.71-21.54-22-22.82-2.75-44.35,11.68-49.71,34.18a192.25,192.25,0,0,0-4.77,53.95c.65,13.5,1.71,16.46,16.15,17.2a221.09,221.09,0,0,0,52.11-3.49c5.93-1.1,9.45-5.62,9.81-11.66C339.85,465.65,340.21,456.68,340.85,443.53Zm-170-.46c.65,12.19,1.24,20.65,1.5,29.11.3,9.31,4.38,13.92,13.76,15.09,17,2.13,33.93,4,51.13,2.2,10.35-1.06,12.52-2.89,13.15-13.28,1-17-.36-33.81-3.08-50.58-4.15-25.52-22.06-41.32-47-41.25-13.46,0-24,8.77-25.78,22.24C172.78,419.91,171.83,433.33,170.89,443.07Zm246.41.43c-.39-8.33-.22-14.54-1.07-20.61A262.42,262.42,0,0,0,411,395.05c-1.77-6.95-7.34-10.41-14.58-10.66a60.85,60.85,0,0,0-8,0c-18.28,1.75-35.5,15.47-37.93,32.89-.79,5.67,0,11.55-.12,17.33-.25,12.31-.65,24.61-.93,36.92-.15,7.14.89,8.39,8.1,8.67,6.79.27,13.74.92,20.37-.13,10.1-1.61,20.06-4.31,29.95-7a10,10,0,0,0,7.67-9.43C416.12,456.22,416.84,448.78,417.3,443.5ZM93.84,441.13l.67.1c.34,6.3.75,12.61,1,18.92.29,7.4,3.86,12.3,11,14.13,8.51,2.16,17,4.63,25.69,5.76,7.34,1,14.91.5,22.35.1,3.11-.16,7.69.3,7.7-4.95,0-17.94-.07-35.89-.14-53.83a11.23,11.23,0,0,0-.32-2c-3.3-18.3-17.84-32.81-36.39-34.72-17.53-1.81-24,1.2-26.9,18.44C96.43,415.69,95.37,428.45,93.84,441.13Zm386-16.88c-.6-8.32-.9-15.13-1.62-21.89-1.16-10.83-5.64-16.59-13.55-18-17-3-38.08,14.48-38.26,32-.15,14.77.11,29.55-.14,44.33-.11,6.49-.15,7.19,6.24,7.46A84.63,84.63,0,0,0,470,461.37c4.44-1.9,6.84-5.08,7.29-9.81C478.23,442,479.13,432.4,479.89,424.25Zm-395.5-4.44c2.68-13.39-10.29-31.77-28.5-35.39-11.8-2.34-19,2.26-21.5,14.08-3.46,16.14-1.75,32.38-.74,48.65.65,10.37,6.57,14.79,15.56,17.12,10.5,2.72,21,5,32,3.62,3-.35,5.09-1,4.5-5.3a204.4,204.4,0,0,1-1.26-21.88C84.24,433.75,84.39,426.78,84.39,419.81Z"
+                      transform="translate(-1.16 -0.82)"></path>
+                <path d="M256,4.34h222.5c18.57,0,29,10.25,28.85,28.67-.06,9.3.33,18.73-1,27.88-2.28,15.43-13.81,26.86-29.21,29.59-12.08,2.14-24.32,3.45-36.52,4.88-3.27.38-5.07,1.08-5.37,4.88-.64,8.28-1.9,16.5-2.78,24.76s-5.87,13.27-14.32,13.63c-4,.17-8,.16-12-.06-7.55-.41-12.84-5.31-13.69-12.71s-1.58-14.54-2.1-21.83c-.3-4.09-1.86-5.77-6.21-5.67q-50.19,1.23-100.4,2.06c-3.83.07-5.11,1.62-5.41,5.38-.68,8.44-1.61,16.87-2.9,25.24-1.13,7.27-5.64,11.1-12.93,11.5a124.89,124.89,0,0,1-13,0c-7.6-.39-12.12-4.34-13.21-11.78-1.19-8-2.38-16.13-2.8-24.24-.25-5-2.21-6.35-6.73-6.13s-9.33.06-14,0q-35.73-.5-71.46-1.08c-4.65-.08-9.31-.45-13.94-.92-3.81-.39-5.53,1.24-5.87,4.92-.63,7-1.52,13.88-2.07,20.84-.7,8.92-5.71,14.09-14.6,14.47-4,.17-8,.15-12-.07-7.71-.41-12.68-5.5-13.52-13.34-.91-8.43-2.15-16.82-2.84-25.26-.29-3.6-2-4.22-5.09-4.59-12.68-1.54-25.41-2.87-38-5.13-14.56-2.61-26.82-16-28-30.81-.87-11-1.26-22.33-.06-33.31C6.9,12.47,17.51,4.39,31.55,4.35c21.83-.06,43.66,0,65.5,0Zm-.13,10.21q-109.49,0-219,0A62.23,62.23,0,0,0,27,15.4,13.45,13.45,0,0,0,15.44,28.93q-.13,8.74,0,17.49c.08,4.82-.46,9.84.71,14.4C19.88,75.39,31,81,44.79,82.21c25.16,2.22,50.33,4.48,75.54,5.85,23.77,1.3,47.59,1.87,71.39,2.06,42,.32,84,.75,126-.08,41.42-.81,82.81-2.89,124.19-5a216.09,216.09,0,0,0,34.48-4.89c11.26-2.42,18.48-10.1,19.91-21.67,1.09-8.71,1-17.6.93-26.4,0-10.59-4.91-15.92-15.34-17.33a52.51,52.51,0,0,0-7-.23Zm.17,86.07c-3,0-6.07-.38-9,.17-1.37.26-3.51,2.37-3.42,3.5.64,8.11,1.58,16.2,2.9,24.22.24,1.46,2.83,3.73,4.14,3.6,4.82-.47,10.36,2.71,14.38-2.3a5,5,0,0,0,.5-2.42c.61-5.11,1.44-10.21,1.72-15.34C268,100.56,267.85,100.55,256.08,100.62Zm-157,27.71c1.66,0,3.57.52,4.93-.12,2-.93,4.72-2.48,5-4.14,1.23-7,1.61-14.16,2.41-21.25.4-3.56-.94-5.24-4.61-5.34-5.47-.14-10.94-.52-16.41-.8-2.52-.12-3.61.81-3.25,3.58.83,6.41,1.25,12.88,2.13,19.28C90.48,128.36,90.6,128.34,99.05,128.33Zm313.08,0c1.83,0,3.9.54,5.42-.14,1.82-.8,4.29-2.48,4.54-4.07,1.22-8,1.93-16.13,2.47-24.23.06-1-1.94-3-3-3-5.63,0-11.26.44-16.9.69-3,.13-4.61,1.41-4.23,4.66.69,5.76,1.31,11.52,1.94,17.29C403.33,128.34,403.32,128.34,412.13,128.32Z"
+                      transform="translate(-1.16 -0.82)"></path>
+                <path d="M256.64,203.18c7.37.12,12.23,5.47,16.71,11.07.79,1,.27,4.41-.76,5.15a7.79,7.79,0,0,1-6.15,1c-2-.67-3.67-2.75-5.36-4.34-3.35-3.17-6.61-3.38-10.06-.13-1.81,1.71-3.61,3.73-5.81,4.61-1.64.65-4.54.36-5.73-.74s-1.82-4.56-1-5.54C243.08,208.82,247.82,203.22,256.64,203.18Z" transform="translate(-1.16 -0.82)"></path>
+                <path d="M99.32,188.33c7.94.57,13,5.73,17.47,11.29.88,1.08,0,4.67-1.25,6-1,1.06-4.06,1.11-5.66.39-2.18-1-3.87-3.07-5.69-4.77-3.26-3-6.42-2.92-9.66.05-1.83,1.68-3.6,3.72-5.79,4.62-1.64.67-4.59.45-5.74-.64s-1.86-4.6-1-5.66C86.46,194,91.63,189.07,99.32,188.33Z" transform="translate(-1.16 -0.82)"></path>
+                <path d="M412.6,198.9a38.46,38.46,0,0,0-4.41,2.39c-2.15,1.57-3.93,3.83-6.28,4.86a6.66,6.66,0,0,1-5.71-.62c-1.16-.89-1.83-3.68-1.36-5.22,2.38-7.76,16-13.94,23.37-10.25,4.2,2.09,7.93,5.51,11.15,9,1.07,1.16.7,5.18-.52,6.46s-4.49,1-6.44.22-3.47-3-5.33-4.43A32,32,0,0,0,412.6,198.9Z" transform="translate(-1.16 -0.82)"></path>
+                <path d="M250.45,241.52c0-3.82-.65-7.8.22-11.41.58-2.41,3-5.64,5.16-6.11,3.39-.75,5.28,2.63,5.38,5.8.25,8,.26,16,0,23.92-.1,2.88-1.48,5.58-5.15,5.64-4,.06-5.19-2.78-5.45-5.88-.35-4-.09-8-.09-12Z" transform="translate(-1.16 -0.82)"></path>
+                <path d="M94.37,227c0-4,0-8,0-11.94,0-3.28,1.55-5.59,5-5.59s5,2.3,5,5.55q.11,12.19,0,24.39c0,3.27-1.79,5.68-5,5.67s-5-2.22-5-5.63C94.38,235.25,94.37,231.1,94.37,227Z" transform="translate(-1.16 -0.82)"></path>
+                <path d="M417.46,227.55v11.94c0,3.36-1.75,5.61-5,5.63s-5-2.4-5-5.64c-.07-8.13-.05-16.26,0-24.39,0-3.33,1.81-5.54,5.08-5.59s4.93,2.33,5,5.61C417.5,219.26,417.46,223.4,417.46,227.55Z" transform="translate(-1.16 -0.82)"></path>
+            </symbol>
+        </svg>
+        <div class="container">
+            <h2 class="h2 mb-2 text-center">What makes Invisalign® different?</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="icon-wrp mb-2">
+                        <svg class="icon" style="font-size: 1.25em;">
+                            <use xlink:href="#key"></use>
+                        </svg>
+                    </div>
+                    <div class="mw-295">
+                        <p class="mb-1"><strong class="fsz-18">Leading the way with teeth-straightening solutions: </strong><br>
+                            Invisalign® is continuously innovative and really is at the forefront of invisible teeth-straightening. Invisalign® is a series of clear removable aligners that straighten your teeth, little by little, and unlike fixed braces there no wires or brackets. This means there is no metal and they are nearly invisible when you wear them.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-wrp mb-2">
+                        <svg class="icon" style="font-size: 1.25em;">
+                            <use xlink:href="#heart"></use>
+                        </svg>
+                    </div>
+                    <div class="mw-295">
+                        <p class="mb-1"><strong class="fsz-18">A completely convenient option for you:</strong><br>
+                            Invisalign® aligners can be removed at any time during treatment, you can eat whatever you want, maintain good oral hygiene, and choose to remove them briefly for special occasions such as weddings or big meetings. Your lifestyle remains intact with Invisalign® invisible aligners.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-wrp mb-2">
+                        <svg class="icon" style="font-size: 1.2em;">
+                            <use xlink:href="#bridges"></use>
+                        </svg>
+                    </div>
+                    <div class="mw-295">
+                        <p><strong class="fsz-18">Teeth-straightening answers for adult teeth:</strong><br>
+                            More adults are considering teeth-straightening than ever before, and the field has new solutions to accommodate this growing need. The primary concern amongst adults is the impact that traditional, metal braces have on appearance. This is only one of the reasons why Invisalign® is such an attractive solution. With near-invisible aligners that are also removable, the impact on your lifestyle is kept to the bare minimum while maintaining the efficacy of the treatment.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="what-next py-4">
+        <div class="container text-center">
+            <h2 class="h2 mb-2">What next?</h2>
+            <p class="mb-2">If you are suffering from twisted and crowded teeth and you are interested in Invisalign, please complete the complimentary consultation form to see if you are suitable.</p>
+            <p class="mb-2 p"><a href="https://www.confidentalsmiles.co.uk/gallerycategory/invisalign/" class="fw-600">Smile Gallery</a> and <a href="https://www.confidentalsmiles.co.uk/testimonials" class="fw-600">Success Stories</a></p>
+            <p><a href="https://www.confidentalsmiles.co.uk/wp-content/uploads/2022/06/TSB052022_TeethStraightening.pdf" class="btn btn-primary">View our Brochure</a></p>
+        </div>
+    </div>
+    <div class="pt-5"></div>
+<?php get_template_part('template-parts/lp-contact'); ?>
+<?php get_template_part('template-parts/lp-treatments'); ?>
+<?php get_template_part('template-parts/lp-glamour-evening'); ?>
+<?php get_template_part('template-parts/lp-workingfeedback'); ?>
+<?php get_template_part('template-parts/lp-footer'); ?>
+<?php
+echo ' </div>';
+get_template_part('template-parts/svg-sprite');
+get_footer();
